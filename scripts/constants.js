c73492638cb124c56e1b7645c2094f4a2df1530f
IlyaFly18 <idaleshin180503@gmail.com>
2022-09-06 12:52:31 +0300

    написал основные классы и объявил классы режимов редактора

---
+++ scripts/constants.js
@@ -0,0 +1,13 @@
+const CANVAS = {
+    id: 'canvas',
+}
+
+const TOOLBAR = {
+    id: 'toolbar',
+    btnClass: 'toolbar-btn',
+    activeBtnClass: 'toolbar-btn-active',
+}
+
+const GLOBALPARAMS = {
+    id: 'global-params',
+}
---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+const $canvas = document.querySelector(CANVAS.id);
+
+const canvas = new Canvas($canvas)
+canvas.start();
